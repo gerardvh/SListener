729f2f77d9d75152a004a9620aa71fa97945afc6
Gerard Van Halsema <gerardvh@me.com>
2015-08-12 12:45:04 -0400

    Change double-quotes to single-quotes

--- sl_listener.rb
+++ sl_listener.rb
@@ -3,24 +3,24 @@ require 'json'
 
 get '/config/incident' do
   config = {
-    name: "SListener",
-    description: "An add-on that listens for ServiceLink incidents and returns a structured and useful response to HipChat.",
-    key: "com.gerardvh.sl_incident_listener",
+    name: 'SListener',
+    description: 'An add-on that listens for ServiceLink incidents and returns a structured and useful response to HipChat.',
+    key: 'com.gerardvh.sl_incident_listener',
     links: {
-        "homepage": "https://gerardvh.com",
-        "self": "https://sl-listener.herokuapp.com/config/incident"
+        homepage: 'https://gerardvh.com',
+        self: 'https://sl-listener.herokuapp.com/config/incident'
     },
     capabilities: {
     hipchatApiConsumer: {
         scopes: [
-            "send_notification"
+            'send_notification'
         ]
     },
     webhook: [{
-        url: "https://sl-listener.herokuapp.com/api/incident/",
-        pattern: "[INCinc]+[0-9]{7}",
-        event: "room_message",
-        name: "incident_listener"
+        url: 'http://96a4b9ae.ngrok.io/api/incident',
+        pattern: '[INCinc]+[0-9]{7}',
+        event: 'room_message',
+        name: 'incident_listener'
         }]
     }
   }
@@ -29,37 +29,37 @@ end
 
 get '/config/kb' do
   config = {
-    name: "KBot",
-    description: "An add-on that listens for ServiceLink knowledge article numbers and returns a structured and useful response to HipChat.",
-    key: "com.gerardvh.sl_kb_listener",
+    name: 'KBot',
+    description: 'An add-on that listens for ServiceLink knowledge article numbers and returns a structured and useful response to HipChat.',
+    key: 'com.gerardvh.sl_kb_listener',
     links: {
-        "homepage": "https://gerardvh.com",
-        "self": "https://sl-listener.herokuapp.com/config/kb"
+        homepage: 'https://gerardvh.com',
+        self: 'https://sl-listener.herokuapp.com/config/kb'
     },
     capabilities: {
     hipchatApiConsumer: {
         scopes: [
-            "send_notification"
+            'send_notification'
         ]
     },
     webhook: [{
-        url: "https://sl-listener.herokuapp.com/api/kb/",
-        pattern: "[KBkb]+[0-9]{7}",
-        event: "room_message",
-        name: "KBot"
+        url: 'http://96a4b9ae.ngrok.io/api/kb',
+        pattern: '[KBkb]+[0-9]{7}',
+        event: 'room_message',
+        name: 'KBot'
         }]
     }
   }
   JSON.dump(config)
 end
 
-post "/api/incident" do
+post '/api/incident' do
   request.body.rewind
   data = JSON.parse request.body.read
   logger.info "Got this as a request body: #{data}"
 end
 
-post "/api/kb" do
+post '/api/kb' do
   request.body.rewind
   data = JSON.parse request.body.read
   logger.info "Got this as a request body: #{data}"
