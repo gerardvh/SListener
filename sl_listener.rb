d180a02dd14e2c50bd4a501ea48c33cc28250ac0
Gerard Van Halsema <gerardvh@me.com>
2015-12-11 13:46:39 -0500

    Truncate loops for maintainability.

--- sl_listener.rb
+++ sl_listener.rb
@@ -38,74 +38,101 @@ def api_all_helper request
   end
   
   # Encapsulate scanning for relevant strings
-  incident_numbers = Incident.scan_for_matches(message)
-  kb_numbers = Knowledge.scan_for_matches(message)
+  # incident_numbers = Incident.scan_for_matches(message)
+  # kb_numbers = Knowledge.scan_for_matches(message)
+
+  numbers = {
+    incident: Incident.scan_for_matches(message),
+    kb: Knowledge.scan_for_matches(message)
+  }
+
+  query_numbers = {
+    incident: [],
+    kb: []
+  }
 
   all_items = {
     incident: [],
     kb: []
   }
 
-  inc_to_query = []
-  kb_to_query = []
+  # inc_to_query = []
+  # kb_to_query = []
 
-  incident_numbers.each do |inc|
-    if incident = REDIS.get(inc) # Check if it is saved in our db
-      # Redis stores all items as a string, so we have it serialized as json
-      all_items[:incident] << JSON.parse(incident)
-    else
-      # If we don't have it in the DB, get ready to query SL
-      inc_to_query << inc
-    end
-  end
-
-  kb_numbers.each do |kb_number|
-    if kb = REDIS.get(kb_number) # Check if exists in db
-      all_items[:kb] << JSON.parse(kb)
-    else
-      kb_to_query << kb_number
-    end
-  end
-
-  unless inc_to_query.empty?
-    incs_from_sl = sl.query(Incident.table, inc_to_query)
-    unless incs_from_sl.empty? # Make sure we got a response from SL
-      incs_from_sl.each do |inc|
-        # Add to our active hash
-        all_items[:incident] << inc
-        # Add to redis store
-        REDIS.set(inc['number'], inc.to_json)
+  numbers.each_pair do |table, nums|
+    nums.each do |num|
+      if item = REDIS.get(num)
+        all_items[table] << JSON.parse(item)
+      else
+        query_numbers[table] << num
       end
     end
   end
 
-  unless kb_to_query.empty?
-    kbs_from_sl = sl.query(Knowledge.table, kb_to_query)
-    unless kbs_from_sl.empty? # Make sure we got a response from SL
-      kbs_from_sl.each do |kb|
-        # Add to our active hash
-        all_items[:kb] << kb
-        # Add to redis store
-        REDIS.set(kb['number'], kb.to_json)
-      end
+  query_numbers.each_pair do |table, nums|
+    items_from_sl = []
+    case table
+    when :incident
+      items_from_sl = sl.query(Incident.table, nums)
+    when :kb
+      items_from_sl = sl.query(Knowledge.table, nums)
+    end
+
+    items_from_sl.each do |item|
+      all_items[table] << item
+      REDIS.set(item['number'], item.to_json)
     end
   end
+
+
+
+
+  # incident_numbers.each do |inc|
+  #   if incident = REDIS.get(inc) # Check if it is saved in our db
+  #     # Redis stores all items as a string, so we have it serialized as json
+  #     all_items[:incident] << JSON.parse(incident)
+  #   else
+  #     # If we don't have it in the DB, get ready to query SL
+  #     inc_to_query << inc
+  #   end
+  # end
+
+  # kb_numbers.each do |kb_number|
+  #   if kb = REDIS.get(kb_number) # Check if exists in db
+  #     all_items[:kb] << JSON.parse(kb)
+  #   else
+  #     kb_to_query << kb_number
+  #   end
+  # end
+
+  # unless inc_to_query.empty?
+  #   incs_from_sl = sl.query(Incident.table, inc_to_query)
+  #   unless incs_from_sl.empty? # Make sure we got a response from SL
+  #     incs_from_sl.each do |inc|
+  #       # Add to our active hash
+  #       all_items[:incident] << inc
+  #       # Add to redis store
+  #       REDIS.set(inc['number'], inc.to_json)
+  #     end
+  #   end
+  # end
+
+  # unless kb_to_query.empty?
+  #   kbs_from_sl = sl.query(Knowledge.table, kb_to_query)
+  #   unless kbs_from_sl.empty? # Make sure we got a response from SL
+  #     kbs_from_sl.each do |kb|
+  #       # Add to our active hash
+  #       all_items[:kb] << kb
+  #       # Add to redis store
+  #       REDIS.set(kb['number'], kb.to_json)
+  #     end
+  #   end
+  # end
 
   items_with_links = {
     incident: [],
     kb: []
   }
-
-  # all_items[:incident].each do |inc|
-  #   # p inc
-  #   link = Incident.link(inc['sys_id'])
-  #   items_with_links[:incident] << Incident.new(inc['number'], link, inc['short_description'])
-  # end
-
-  # all_items[:kb].each do |kb|
-  #   link = Knowledge.link(kb['number'])
-  #   items_with_links[:kb] << Knowledge.new(kb['number'], link, kb['short_description'])
-  # end
 
   all_items.each_key do |table|
     all_items[table].each do |sl_item|
