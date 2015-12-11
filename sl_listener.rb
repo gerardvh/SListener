2f022e8be23de4f1903be3467168e5ce9c56ec5d
Gerard Van Halsema <gerardvh@me.com>
2015-12-11 14:09:46 -0500

    Copy template hash instead of re-writing, and remove commented out code.

--- sl_listener.rb
+++ sl_listener.rb
@@ -37,28 +37,24 @@ def api_all_helper request
     return {}
   end
   
+  template = {
+    incident: [],
+    kb: []
+  }
+  # Copying our template to reduce code duplication
+  numbers = Hash.new.merge(template)
+  query_numbers = Hash.new.merge(template)
+  all_items = Hash.new.merge(template)
+  items_with_links = Hash.new.merge(template)
+
   # Encapsulate scanning for relevant strings
-  # incident_numbers = Incident.scan_for_matches(message)
-  # kb_numbers = Knowledge.scan_for_matches(message)
+  numbers[:incident] = Incident.scan_for_matches(message)
+  numbers[:kb] = Knowledge.scan_for_matches(message)
 
-  numbers = {
-    incident: Incident.scan_for_matches(message),
-    kb: Knowledge.scan_for_matches(message)
-  }
 
-  query_numbers = {
-    incident: [],
-    kb: []
-  }
-
-  all_items = {
-    incident: [],
-    kb: []
-  }
-
-  # inc_to_query = []
-  # kb_to_query = []
-
+  # TODO: Add comments and log statements
+  # maybe keep track of how many times we pull from the cache?
+  # also maybe track how many un-supported regex responses we get?
   numbers.each_pair do |table, nums|
     nums.each do |num|
       if item = REDIS.get(num)
@@ -77,65 +73,14 @@ def api_all_helper request
     when :kb
       items_from_sl = sl.query(Knowledge.table, nums)
     end
-
     items_from_sl.each do |item|
       all_items[table] << item
       REDIS.set(item['number'], item.to_json)
     end
   end
 
-
-
-
-  # incident_numbers.each do |inc|
-  #   if incident = REDIS.get(inc) # Check if it is saved in our db
-  #     # Redis stores all items as a string, so we have it serialized as json
-  #     all_items[:incident] << JSON.parse(incident)
-  #   else
-  #     # If we don't have it in the DB, get ready to query SL
-  #     inc_to_query << inc
-  #   end
-  # end
-
-  # kb_numbers.each do |kb_number|
-  #   if kb = REDIS.get(kb_number) # Check if exists in db
-  #     all_items[:kb] << JSON.parse(kb)
-  #   else
-  #     kb_to_query << kb_number
-  #   end
-  # end
-
-  # unless inc_to_query.empty?
-  #   incs_from_sl = sl.query(Incident.table, inc_to_query)
-  #   unless incs_from_sl.empty? # Make sure we got a response from SL
-  #     incs_from_sl.each do |inc|
-  #       # Add to our active hash
-  #       all_items[:incident] << inc
-  #       # Add to redis store
-  #       REDIS.set(inc['number'], inc.to_json)
-  #     end
-  #   end
-  # end
-
-  # unless kb_to_query.empty?
-  #   kbs_from_sl = sl.query(Knowledge.table, kb_to_query)
-  #   unless kbs_from_sl.empty? # Make sure we got a response from SL
-  #     kbs_from_sl.each do |kb|
-  #       # Add to our active hash
-  #       all_items[:kb] << kb
-  #       # Add to redis store
-  #       REDIS.set(kb['number'], kb.to_json)
-  #     end
-  #   end
-  # end
-
-  items_with_links = {
-    incident: [],
-    kb: []
-  }
-
-  all_items.each_key do |table|
-    all_items[table].each do |sl_item|
+  all_items.each_pair do |table, nums|
+    nums.each do |sl_item|
       case table
       when :incident
         items_with_links[table] << Incident.new(sl_item['number'], Incident.link(sl_item['sys_id']), sl_item['short_description'])
@@ -147,9 +92,3 @@ def api_all_helper request
 
   return items_with_links 
 end
-
-
-# {incident: 'incident',
-#     kb: 'kb_knowledge',
-#     task: 'sc_task',
-#     ritm: 'sc_req_item'}
