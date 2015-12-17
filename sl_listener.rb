3bcc619a3cc1ac907774265235b0fe62af9fadec
Gerard Van Halsema <gerardvh@me.com>
2015-12-17 16:52:04 -0500

    Move files to /lib. Refactor api_all_helper.

--- sl_listener.rb
+++ sl_listener.rb
@@ -1,16 +1,19 @@
+# Add ./lib to the load-path
+$: << "./lib"
+
 require 'sinatra'
 require 'tilt/erubis'
 require 'json'
 require 'redis'
 require 'uri'
-
-require_relative('hipchat_config')
-require_relative('sl_helper')
-require_relative('sl_items')
+# From ./lib
+require('hipchat_config')
+require('sl_helper')
+require('sl_items')
 
 #### Setup REDIS ####
 # Use the ENV var with our redis url by not including any arguments.
-REDIS = Redis.new
+$redis = Redis.new
 
 get '/config/all' do
   return Hipchat_helper.all_config
@@ -18,30 +21,7 @@ end
 
 post '/api/all' do
   # Increment a count of how many times this API is accessed
-  REDIS.incr('api:all:attempts')
-  items = api_all_helper(request)
-  if items.empty?
-    REDIS.incr('api:all:unsupported')
-    break
-  else
-    # Increment a count of how many individual items have been returned
-    items.values.each do |array|
-      array.each { REDIS.incr('api:all:items_returned') }
-    end
-    html_message = erb :hipchat_kb, locals: items
-    return Hipchat_helper.return_message(html_message)
-  end
-end
-
-def template
-  return {
-    Incident.table => [],
-    Knowledge.table => []
-  }
-end
-
-def api_all_helper request
-  sl = Sl_helper.new
+  $redis.incr('api:all:attempts')
   # rewind in case it was read by something else
   request.body.rewind
   # get what the user said in chat
@@ -49,56 +29,93 @@ def api_all_helper request
     message = JSON.parse(request.body.read)['item']['message']['message']
   rescue Exception => e
     # Return early if we can't parse the JSON from Hipchat
-    return {}
+    $redis.incr('api:failure:message_parse')
+    return
   end
   
   # Copying our template to reduce code duplication
   # The template method should return a fresh hash with service link table-names
   # as the keys and empty arrays as values.
-  numbers = template
-  query_numbers = template
-  all_items = template
-  items_with_links = template
+  numbers = template()
 
-  # Encapsulate scanning for relevant strings
+  tasks = Task.scan_for_matches(message)
+  unless tasks.empty?
+    tasks.each do |task_number|
+      $redis.sadd('api:unsupported:task', task_number)
+    end
+  end
+
+  requests = Request.scan_for_matches(message)
+  unless requests.empty?
+    requests.each do |req_number|
+      $redis.sadd('api:unsupported:request', req_number)
+    end
+  end
+
   numbers[Incident.table] = Incident.scan_for_matches(message)
   numbers[Knowledge.table] = Knowledge.scan_for_matches(message)
 
-  # TODO: Add comments and log statements
-  # maybe keep track of how many times we pull from the cache?
-  # also maybe track how many un-supported regex responses we get?
+  if numbers[Incident.table].empty? && numbers[Knowledge.table].empty?
+    return # Early return if we have no supported items to parse
+  end
+
+  cached_items, items_to_query = separate_cached_items(numbers)
+  all_items = combine_cache_and_query(cached_items, items_to_query)
+  items_with_links = add_links(all_items)
+
+  items.values.each do |array|
+    array.each { $redis.incr('api:all:items_returned') }
+  end
+
+  html_message = erb :hipchat_kb, locals: items
+  return Hipchat_helper.return_message(html_message)
+end
+
+def template
+  # This should return a hash with all the supported SL Tables as keys, and empty arrays as values
+  return {
+    Incident.table => [],
+    Knowledge.table => []
+  }
+end
+
+def separate_cached_items numbers=template(), cached_items=template(), items_to_query=template()
   numbers.each_pair do |table, nums|
     nums.each do |num|
-      if item = REDIS.get(num)
-        all_items[table] << JSON.parse(item)
+      if item = $redis.get(num)
+        cached_items[table] << JSON.parse(item)
       else
-        query_numbers[table] << num
+        items_to_query[table] << num
       end
     end
   end
+  return cached_items, items_to_query
+end
 
-  query_numbers.each_pair do |table, nums|
+def combine_cache_and_query cached_items=template(), items_to_query=template()
+  sl = Sl_helper.new # Create a helper for querying Service Link
+  items_to_query.each_pair do |table, nums|
     items_from_sl = sl.query(table, nums)
     items_from_sl.each do |item|
-      all_items[table] << item
-      REDIS.set(item['number'], item.to_json)
+      cached_items[table] << item
+      # Set our cache for queried items with keys like "incident:INC098219"
+      $redis.set("#{table}:#{item['number']}", item.to_json)
     end
   end
+  return cached_items # Which now also has the newly queried items as well
+end
 
-  all_items.each_pair do |table, sl_items|
+def add_links items
+  items_with_links = template()
+  items.each_pair do |table, sl_items|
     sl_items.each do |item|
-      p "table = #{table}"
       case table
       when Incident.table
-        # Maybe I can override item init to take a table parameter and do the logic there?
-        p items_with_links
-        
         items_with_links[table] << Incident.new(item['number'], Incident.link(item['sys_id']), item['short_description'])
       when Knowledge.table
         items_with_links[table] << Knowledge.new(item['number'], Knowledge.link(item['number']), item['short_description'])
       end
     end
   end
-
-  return items_with_links 
+  return items_with_links
 end
